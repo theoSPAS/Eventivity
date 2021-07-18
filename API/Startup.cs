b4ac2e02a1dc44b254cdc7774664f797150ddae7
theoSPAS <spasovska.t@yahoo.com>
2021-07-18 20:01:56 +0200

    Creating CRUD functionalities using CQRS and Mediator pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,11 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
+using AutoMapper;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -30,23 +35,8 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opt => 
-            {
-                opt.AddPolicy("CorsPolicy", policy => 
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            
+            services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
