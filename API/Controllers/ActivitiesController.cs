1e83748d349933cca98558af98977d30f0140e30
theoSPAS <spasovska.t@yahoo.com>
2021-08-02 18:37:03 +0200

    Client side login and registration

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,9 +3,11 @@ using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
