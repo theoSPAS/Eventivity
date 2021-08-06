e6f1e3997fb1c6446be29f715d47789b7c6ad976
theoSPAS <spasovska.t@yahoo.com>
2021-08-06 12:31:24 +0200

    SignalR

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -31,7 +31,8 @@ namespace API.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy => 
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+                    .WithOrigins("http://localhost:3000");
                 });
             });
 
@@ -40,10 +41,11 @@ namespace API.Extensions
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
             services.AddScoped<IUserAccessor, UserAccessor>();
-
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
 
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+
+            services.AddSignalR();
 
             return services;
         }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -17,22 +17,29 @@ namespace Persistence
 
         public DbSet<Photo> Photos {get; set;}
 
+        public DbSet<Comment> Comments {get; set;}
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<ActivityAttendee >(x => 
-            x.HasKey( z => new {z.AppUserId, z.ActivityId}));
+            builder.Entity<ActivityAttendee>(x =>
+            x.HasKey(z => new { z.AppUserId, z.ActivityId }));
 
             builder.Entity<ActivityAttendee>()
             .HasOne(u => u.AppUser)
             .WithMany(t => t.Activities)
             .HasForeignKey(m => m.AppUserId);
 
-             builder.Entity<ActivityAttendee>()
-            .HasOne(u => u.Activity)
-            .WithMany(t => t.Attendees)
-            .HasForeignKey(m => m.ActivityId);
+            builder.Entity<ActivityAttendee>()
+           .HasOne(u => u.Activity)
+           .WithMany(t => t.Attendees)
+           .HasForeignKey(m => m.ActivityId);
+
+            builder.Entity<Comment>()
+            .HasOne(x => x.Activiy)
+            .WithMany(u => u.Comments)
+            .OnDelete(DeleteBehavior.Cascade);
         }
 
              
